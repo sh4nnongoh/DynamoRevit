01621a1492269cd5dc864b4cbd64240b288c165f
Shannon <sh4nnongoh@gmail.com>
2016-03-01 11:04:11 +0800

    Edited the installer

--- tools/install/CreateInstallers.bat
+++
@@ -1,9 +0,0 @@
-SET cwd=%0\..
-echo %cwd%
-
-copy %cwd%\..\..\src\DynamoRevitInstall\bin\x86\Release\DynamoRevitInstall.msi %cwd%\temp
-copy %cwd%\..\..\..\Dynamo\src\DynamoInstall\bin\x86\Release\DynamoInstall.msi %cwd%\temp
-
-"C:\Program Files (x86)\Inno Setup 5\iscc.exe" %cwd%\DynamoRevitInstaller.iss
-
-rmdir /Q /S %cwd%\temp
